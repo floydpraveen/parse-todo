d4ad739583ce3fb7de33cbc11c1dce9c26023af9
floydpraveen <pdnsjce@gmail.com>
2013-08-15 02:58:37 +0530

    added test for save functionality of model

--- js/tests/models/todo_test.js
+++ js/tests/models/todo_test.js
@@ -1,11 +1,25 @@
 
 define(
-    ['../../models/todo','sinonQunit'],
-    function(Todo, sinonQunit) {
+    ['../../models/todo',  'sinonQunit','../../utils/helper'],
+    function(Todo,  sinonQunit, helper) {
         
         var run = function() {
 
-        	module('About TODO Model');
+           module('About TODO Model', {
+
+				setup: function() {
+					this.requests = [];
+					this.xhr = sinon.useFakeXMLHttpRequest();
+					this.xhr.onCreate = $.proxy(function(xhr) {
+						this.requests.push(xhr);
+					}, this);
+				},
+				teardown: function() {
+					this.xhr.restore();
+				}
+
+			});
+
            test('init of model shd set id using object id', function() {
 			    expect(1);
 			    var todo = new Todo({'objectId':100});
@@ -21,6 +35,19 @@ define(
 			    // Hint: http://documentcloud.github.com/backbone/#Model-set
 			    todo.set({ text: "new text" });
 			    ok(spy.calledOnce, "A change event callback was correctly triggered");
+			});
+
+            test('model should be able to save the state', function() {
+			    expect(3);
+			    var todo = new Todo();
+			    todo.set('done',true);
+			    todo.set('id',100);
+			    todo.url = helper.todosUrl;
+			    todo.save();
+			    var request = this.requests[0];
+			    equal(request.method, 'PUT', 'TestModel#save should send a PUT');
+			    equal(request.url, helper.todosUrl, '... to /api/testmodel/ endpoint');
+			    equal(JSON.parse(request.requestBody).done, true, '... with valid SMTP json');			    
 			});
 
         };        
