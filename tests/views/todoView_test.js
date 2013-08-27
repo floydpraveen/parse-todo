87d5690d7a6d774adde3dcd2383ff2658526e86b
floydpraveen <pdnsjce@gmail.com>
2013-08-27 12:56:13 +0530

    setup and tear function added to create view

--- tests/views/todoView_test.js
+++ tests/views/todoView_test.js
@@ -1,86 +1,74 @@
 define(
-  ['js/models/todo', 'js/views/todoView', 'helper'],
-  function(Todo, TodoView, helper) {
+    ['js/models/todo', 'js/views/todoView', 'helper'],
+    function(Todo, TodoView, helper) {
 
-    var run = function() {
+        var run = function() {
 
-      module("TODO VIEW" , {
+            module("TODO VIEW", {
+                setup: function() {
+                  $('<ul></ul>',{
+                    id:'todoList'
+                  }).appendTo('body');
 
-      setup: function() {
-        this.server = sinon.fakeServer.create();
-        this.server.respondWith("POST", helper.todosUrl, [
-          200, {
-            "Content-Type": "application/json"
-          },
-          '[{"id": 0, "done": "Hello World"}]'
-        ]);
+                  this.todoView = new TodoView({
+                      model: new Todo()
+                  });
 
-      },
-      teardown: function() {
-        this.server.restore();
-      }
-    });
+                this.server = sinon.fakeServer.create();
+                this.server.respondWith("POST", helper.todosUrl, [
+                  200, {
+                    "Content-Type": "application/json"
+                  },
+                  '[{"id": 0, "done": "Hello World"}]'
+                ]);
+                },
+                teardown: function() {
+                    this.todoView.remove();
+                    $("#todoList").remove();
+                     this.server.restore();
+                }
+            });
 
-      asyncTest('render on model change', function() {
-        expect(2);
-        var todo = new Todo({
-          title: 'name'
-        });
-        this.view = new TodoView({
-          model: todo
-        });
-        todo.trigger('change');
-        //console.log(this.view.$el.html().length);
-        ///console.log(this.view.$el.find('div').length);
-        equal(this.view.$el.find('div').length, 1, 'view should render it self');
-        ok(this.view.$el.html().length > 0, 'html should be rendered');
-        start();
-      });
+            asyncTest('render on model change', function() {
+                expect(2);    
+                this.todoView.model.set('title','title1');
+                console.log(this.todoView.$el.is('div'));
+                equal(this.todoView.$el.find('div').length, 1, 'view should render it self');
+                ok(this.todoView.$el.html().length > 0, 'html should be rendered');
+                start();
+            });
 
-      test('edit on double clcik', function(){
-         expect(1);
-         var todo = new Todo({
-          title: 'name'
-        });
-        this.view = new TodoView({
-          model: todo
-        });
-        this.view.render();
-        this.view.$el.find('.view').trigger('dblclick');
-        ok(this.view.$el.hasClass('editing'), 'View shd have calss editing after double click');
-      });
+            test('edit on double clcik', function() {
+                expect(1);
+                this.todoView.render();
+                this.todoView.$el.find('.view').trigger('dblclick');
+                ok(this.todoView.$el.hasClass('editing'), 'View shd have calss editing after double click');
+            });
 
-      test('toggle model value when user clciks toggle', function(){
-          expect(1);
-          var todo = new Todo();
-          todo.set('done',false);
-          var todoView = new TodoView({model:todo});
-          todoView.render();
-          todoView.$el.find('.toggle').trigger('click');
-          equal(todoView.model.get('done'), true, 'model value should be toggled');
-      });
+            //THIS TEST CASE FAILS IN CHROME !
+            test('toggle model value when user clciks toggle', function(){                
+                expect(1);
+                this.todoView.model.set({'done':false, 'title': 'name'});
+                this.todoView.render();
+                this.todoView.$el.find('.toggle').trigger('click');
+                equal(this.todoView.model.get('done'), true, 'model value should be toggled');
+            });
 
-      test('after edit save that to model', function(){
-          expect(2);
-         var todo = new Todo({
-          title: 'name'
-        });
-         todo.url = helper.todosUrl;
-        this.view = new TodoView({
-          model: todo
-        });
-        this.view.render();
-        this.view.$el.find('.view').trigger('dblclick');
-        this.view.$el.find('input').val('testing');
-        this.view.$el.find('.edit').trigger('blur');
-        this.server.respond();
-        equal(this.view.$el.hasClass('editing'),false, 'it should not contain editing class');
-        equal(todo.get('title'),'testing','title should be the entered title in the edit field');
-      });
+            test('after edit save that to model', function() {
+                expect(2);
+                this.todoView.model.url = helper.todosUrl;
+                this.todoView.render();
+                this.todoView.$el.find('.view').trigger('dblclick');
+                this.todoView.$el.find('input').val('testing');
+                this.todoView.$el.find('.edit').trigger('blur');
+                this.server.respond();
+                equal(this.todoView.$el.hasClass('editing'), false, 'it should not contain editing class');
+                equal(this.todoView.model.get('title'), 'testing', 'title should be the entered title in the edit field');
+            });
 
 
 
-    };
-    return run
-  }
+        };
+        return run
+    }
 );
