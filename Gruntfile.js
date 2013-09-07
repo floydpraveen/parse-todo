07722288c43e50ff1c2a2e5fdb67c1177f08717a
floydpraveen <pdnsjce@gmail.com>
2013-09-07 14:13:21 +0530

    task to run specs via phantomjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,15 @@
 
 module.exports = function(grunt) {
   grunt.initConfig({
-    // connect: {
-    //   test: {
-    //     port: 8000,
-    //     middleware: function(connect) {
-    //       return [
-    //         mountFolder(connect, 'app')
-    //       ];
-    //     }
-    //   }
-    // },
-
-    // watch: {
-    //   files: ['jasmine/spec/**/*.js', 'app/**/*.js'],
-    //   tasks: 'exec'
-    // },
-
     exec: {
       jasmine: {
-        command: 'phantomjs jasmine/lib/run-jasmine.js jasmine/index.html',
+        command: 'phantomjs tests/lib/phantomjs_jasmine_runner.js tests/index.html tests/',
         stdout: true
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-exec');
- // grunt.loadNpmTasks('grunt-contrib-connect');
- // grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['exec']);
 
